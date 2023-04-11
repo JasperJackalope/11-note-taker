fe8edf137d38096eff94b8e86b795fe72fa79047
JasperJackalope <125604651+JasperJackalope@users.noreply.github.com>
2023-04-10 17:04:54 -0700

    Add App Listener

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require('express');
+const path = require("path")
 const fs = require('fs');
 
-const PORT = 3001;
-
 const app = express();
+const PORT = 30001;
+
+app.listen(PORT, function() {
+    console.log(`App is listening on Port ${PORT}`);
+})
