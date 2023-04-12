468138481a99d6cd9b04f28bad5fbb63f6073460
JasperJackalope <125604651+JasperJackalope@users.noreply.github.com>
2023-04-11 17:13:11 -0700

    Refinded Server JS and Functionality

--- server.js
+++ server.js
@@ -1,15 +1,17 @@
 const express = require('express');
-const path = require("path")
-const dbData = require('./db/db.json');
+const path = require('path');
 const fs = require('fs');
 
 const app = express();
 const PORT = 3001;
 
+// Serve static files from the "public" directory
 app.use(express.static('public'));
-app.use(express.urlencoded({ extended: true }));
+
+// Parse incoming JSON requests
 app.use(express.json());
 
+// Load existing notes from file
 let notes = [];
 fs.readFile('./db/db.json', 'utf8', (err, data) => {
   if (err) {
@@ -19,13 +21,52 @@ fs.readFile('./db/db.json', 'utf8', (err, data) => {
   notes = JSON.parse(data);
 });
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, './public/notes.html'));
+// API endpoints for getting and posting notes
+app.get('/api/notes', (req, res) => {
+  res.json(notes);
 });
 
-app.get('/api/notes', (req, res) => res.json(dbData));
+app.post('/api/notes', (req, res) => {
+  const newNote = {
+    id: notes.length + 1,
+    title: req.body.title,
+    text: req.body.text,
+  };
+  notes.push(newNote);
+  fs.writeFile('./db/db.json', JSON.stringify(notes), 'utf8', err => {
+    if (err) {
+      console.error(err);
+      res.status(500).json({ error: 'Failed to save note' });
+    } else {
+      res.json(newNote);
+    }
+  });
+});
 
-app.listen(PORT, function() {
-    console.log(`App is listening on Port ${PORT}`);
-})
+// Serve the notes page for any other request
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public/notes.html'));
+});
 
+app.delete('/api/notes/:id', (req, res) => {
+  const noteId = parseInt(req.params.id);
+  const index = notes.findIndex(note => note.id === noteId);
+  if (index !== -1) {
+    notes.splice(index, 1);
+    fs.writeFile('./db/db.json', JSON.stringify(notes), 'utf8', err => {
+      if (err) {
+        console.error(err);
+        res.status(500).json({ error: 'Failed to delete note' });
+      } else {
+        res.sendStatus(204);
+      }
+    });
+  } else {
+    res.status(404).json({ error: 'Note not found' });
+  }
+});
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
